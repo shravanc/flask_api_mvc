4a3f1b4763c8d33476cdbc34f53fb031760ad6ce
shravan-infrrd <shravan@infrrd.ai>
2018-12-05 15:45:15 +0530

    removed unused directory

--- config/hello.py
+++
@@ -1,5 +0,0 @@
-import os
-
-yourpath = os.path.abspath(os.path.dirname(__file__))
-print(os.path.abspath(os.path.dirname(__file__)))
-print(os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
